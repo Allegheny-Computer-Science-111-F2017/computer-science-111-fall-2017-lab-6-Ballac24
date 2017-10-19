15e9540c3c5a1eebe156ac94faadf55e89264424
Chase Balla <ballac@aldenv103.allegheny.edu>
2017-10-19 13:41:02 -0400

    Materal for lab 6

--- src/main/java/labsix/ManipulateDna.java
+++ src/main/java/labsix/ManipulateDna.java
@@ -3,13 +3,20 @@ package labsix;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Date;
+import java.util.Random;
 import java.util.Scanner;
 
+/** This program takes a string of DNA and prints the compliment of that DNA.
+   *
+   *
+   */
 public class ManipulateDna {
-
+  /** This program will prompt the user to type a string of DNA.
+   * It will then pring the complement at that DNA.
+   */
   public static void main(String[] args) {
     // display the name of the programmer and the date
-    System.out.println("Gregory M. Kapfhammer " + new Date());
+    System.out.println("Chase Balla " + new Date());
     // declare the starting file and scanner
     File dnaInputsFile = null;
     Scanner scanner = null;
@@ -21,16 +28,51 @@ public class ManipulateDna {
       System.out.println("Unable to locate file");
     }
     // Step One: Read in and display the chosen DNA string
-
+    String input = scanner.nextLine();
+    final String dna = input.toUpperCase();
     // Step Two: Compute the complement of the DNA String
+    String dnacomp = dna;
+    System.out.println("Okay I am going to manipulate the DNA string" + dna);
 
+    dnacomp = dnacomp.replace('T','X');
+    dnacomp = dnacomp.replace('A','Y');
+    dnacomp = dnacomp.replace('G','Z');
+    dnacomp = dnacomp.replace('C','W');
+
+    dnacomp = dnacomp.replace('X','A');
+    dnacomp = dnacomp.replace('Y','T');
+    dnacomp = dnacomp.replace('Z','G');
+    dnacomp = dnacomp.replace('W','C');
+
+    // Printing the compliment 
+    System.out.println("Here is the compliment of the dna sequence: " + dnacomp);
+    System.out.println("Complement of ACTG is " + dnacomp);
     // Step Three: Insert a randomly chosen DNA letter into the DNA String
+    String allowed = "CGTA";
+    Random ran = new Random();
+    int randomNum = ran.nextInt(4);
+    char randomletter = allowed.charAt(randomNum);
+    System.out.println("Inserting " + randomletter + " at position 0 gives " + randomletter + dna);
 
     // Step Four: Delete a DNA letter from a randomly chosen position in the DNA string
+    int randompos = ran.nextInt(dna.length());
+    String dnadelete = ("" + dna.substring(0, randompos)
+        + dna.substring(randompos + 1, dna.length()));
 
+       
+
+    System.out.println("Deleting from postion " + (randompos + 1) + " gives " + dnadelete);
     // Step Five: Change a random position in the DNA String to a randomly chosen DNA letter
+    int randomNum2 = ran.nextInt(dna.length());
+    char randomletter2 = allowed.charAt(randomNum2);
+    int randomNum3 = ran.nextInt(dna.length());
+
+    String dnasub = "" + dna.substring(0, randomNum2) + randomletter2
+        + dna.substring(randomNum2 + 1, dna.length());
+    System.out.println("Changing position " + (randomNum2 + 1) + " gives " + dnasub);
 
     // Step Six: Display a final thankyou message
+    System.out.println("\nThank you for using the ManipulateDna program. \n Have a nice Day! ");
 
   }
 
--- writing/reflection.md
+++ writing/reflection.md
@@ -1,4 +1,21 @@
 # Reflection by
 
-This lab required me to write a file called `ManipulateDNA`. Please review the
-Laboratory Assignment Sheet and add the required details to this paragraph.
+This lab required me to write a file called `ManipulateDNA`. The first was to
+replace the letters within the string of the DNA with their respective
+compliments. When I first tried it I failed. I realized that I was simplying
+replacing the letters within the string with the same letter. This caused the
+letters to remain the same. I fixed this error by replacing each letter with a
+random letter.
+
+The hardest part of this lab assignment was to deleting a random letter. I
+didn't understand how to delete the letters. To get around this dilema I gave
+each letter a different character. When i was doing the lab it wasn't random.
+The compliment wouldn't change. This is because I didn't declare the variable
+"allowed". After I did this I solved my problem and changed the DNA sequence.
+
+I understood how to choose the random character from the string. I didn't
+understand how to get to delete the specific character. To delete it from the
+string I had to create two different substrings that deleted the letter. It was
+a simple step that I was missing. After I did all of the steps needed. I then
+ran the program and found out that it was working. The DNA sequence was
+changing everytime I ran the program.
